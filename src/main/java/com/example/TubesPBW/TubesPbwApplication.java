49616cba34a04eba88a9a0ae3c0754cd455f2105
Naia Azzahra Sugandi <naiaazzahra.2701@gmail.com>
2024-11-28 10:44:23 +0700

    folder gradle

---
+++ src/main/java/com/example/TubesPBW/TubesPbwApplication.java
@@ -0,0 +1,13 @@
+package com.example.TubesPBW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesPbwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesPbwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TubesPBW/TubesPbwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TubesPBW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesPbwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
