3c4e15895c9deaa26c3d3f0af53c7f0211657124
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-08 19:31:54 +0700

    Create skeleton of project.

---
+++ src/main/java/com/PBW/RanTreker/RanTrekerApplication.java
@@ -0,0 +1,13 @@
+package com.PBW.RanTreker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanTrekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanTrekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PBW/RanTreker/RanTrekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PBW.RanTreker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanTrekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
