e99bb74e20f9d98e1e4659d29ebcde3aa4fb68a9
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-08 19:37:32 +0700

    Atur dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,16 +24,19 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation ("org.springframework.boot:spring-boot-starter-validation")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/PBW/RanTreker/RanTrekerApplication.java
+++ src/main/java/com/PBW/RanTreker/RanTrekerApplication.java
@@ -2,10 +2,10 @@ package com.PBW.RanTreker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RanTrekerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RanTrekerApplication.class, args);
 	}
