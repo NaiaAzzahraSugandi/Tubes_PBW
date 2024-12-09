06b09871d355caa6cab7954e5ccb3d5e89dd72a7
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-09 11:13:38 +0700

    Tabel list activity udah bisa dynamic.

--- src/main/java/com/PBW/RanTreker/Activity/ActivityController.java
+++ src/main/java/com/PBW/RanTreker/Activity/ActivityController.java
@@ -1,5 +1,7 @@
 package com.PBW.RanTreker.Activity;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +14,29 @@ import jakarta.validation.Valid;
 
 @Controller
 public class ActivityController {
+    private final HttpSession session;
+
+    public ActivityController(HttpSession session){
+        this.session = session;
+    }
+
     @Autowired
     JDBCActivityRepository activityRepository;
 
     @GetMapping("/activity")
-    public String activityView(){
+    public String activityView(Model model){
+        // mock the session
+        session.setAttribute("id_user", 2);
+        
+        int id_user = (int) session.getAttribute("id_user");
+        List<Activity> activities = activityRepository.findAll(id_user);
+        model.addAttribute("totalActivity", activities.size());
+        model.addAttribute("activities", activities);
         return "activities";
     }
 
     @GetMapping("/activityEntry")
-    public String activityEntryView(Activity activity, HttpSession session, Model model){
-        session.setAttribute("id_user", 2);
+    public String activityEntryView(Activity activity, Model model){
         model.addAttribute("id_user", (Integer)session.getAttribute("id_user"));
         return "entryRun";
     }
