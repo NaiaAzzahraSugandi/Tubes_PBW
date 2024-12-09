807c927dbb5885b1f33d930610bd5939cc66ec7b
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-09 11:15:15 +0700

    Start fitur login.

--- src/main/java/com/PBW/RanTreker/LoginController.java
+++ src/main/java/com/PBW/RanTreker/LoginController.java
@@ -3,10 +3,17 @@ package com.PBW.RanTreker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.PBW.RanTreker.User.User;
+
 @Controller
 public class LoginController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginView(User user){
+        return "login";
     }
 }
