1971978513a15230767011a113d38923dafbeb76
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-10 00:03:35 +0700

    Fix filtering, untuk dropdown time, duration, dan distance hanya 1 yang bisa di apply.

--- src/main/java/com/PBW/RanTreker/Activity/JDBCActivityRepository.java
+++ src/main/java/com/PBW/RanTreker/Activity/JDBCActivityRepository.java
@@ -15,48 +15,59 @@ public class JDBCActivityRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void save(Activity activity){
+    public void save(Activity activity) {
         String sql = "INSERT INTO activities (id_user, title, distance, duration, date, time, description, image_location) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql, 
-                            activity.getId_user(),
-                            activity.getTitle(),
-                            activity.getDistance(),
-                            activity.getDuration(),
-                            activity.getDate(),
-                            activity.getTime(),
-                            activity.getDescription(),
-                            activity.getImage_location());
+        jdbcTemplate.update(sql,
+                activity.getId_user(),
+                activity.getTitle(),
+                activity.getDistance(),
+                activity.getDuration(),
+                activity.getDate(),
+                activity.getTime(),
+                activity.getDescription(),
+                activity.getImage_location());
     }
-    
-    public List<Activity> findAll(Integer id_user,
-                                  String title,
-                                  LocalDate startDate,
-                                  LocalDate endDate,
-                                  String time,
-                                  String duration,
-                                  String distance){
-        String sql = "SELECT * FROM activities WHERE id_user = " + id_user;
 
+    public List<Activity> findAll(Integer id_user,
+            String title,
+            LocalDate startDate,
+            LocalDate endDate,
+            String time,
+            String duration,
+            String distance) {
+        String sql = "SELECT * FROM activities WHERE id_user = ?";
         List<Object> filterList = new ArrayList<>();
-        if(title.length() > 0){
+        filterList.add(id_user); // Add user ID to the filter list
+
+        if (title.length() > 0) {
             sql += " AND title ILIKE ?";
             filterList.add("%" + title + "%");
         }
-        if(startDate != null){
+        if (startDate != null) {
             sql += " AND date >= ?";
             filterList.add(startDate);
         }
-        if(endDate != null){
+        if (endDate != null) {
             sql += " AND date <= ?";
             filterList.add(endDate);
         }
 
-        sql += " ORDER BY time " + time + ", duration " + duration + ", distance " + distance;
-        return jdbcTemplate.query(sql, this::mapRowToActivity, filterList.toArray());
+        // Append ORDER BY clause based on the sorting parameter
+        if (!time.equals("None")) {
+            sql += " ORDER BY time " + time;
+        } 
+        else if (!duration.equals("None")) {
+            sql += " ORDER BY duration " + duration;
+        } 
+        else if (!distance.equals("None")) {
+            sql += " ORDER BY distance " + distance;
+        }
+
+        return jdbcTemplate.query(sql.toString(), this::mapRowToActivity, filterList.toArray());
     }
 
-    public Activity mapRowToActivity(ResultSet resultSet, int rowNum) throws SQLException{
+    public Activity mapRowToActivity(ResultSet resultSet, int rowNum) throws SQLException {
         return new Activity(
                 resultSet.getInt("id"),
                 resultSet.getInt("id_user"),
@@ -67,8 +78,7 @@ public class JDBCActivityRepository {
                 resultSet.getTime("time").toLocalTime(),
                 resultSet.getString("description"),
                 null,
-                resultSet.getString("image_location")
-                );
+                resultSet.getString("image_location"));
     }
 
 }
