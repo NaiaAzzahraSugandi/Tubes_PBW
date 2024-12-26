09f816512ff28fc320710debb695a3dac608c3be
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-26 22:39:44 +0700

    Start implementasi race di page user.

---
+++ src/main/java/com/PBW/RanTreker/Races/RaceUserController.java
@@ -0,0 +1,10 @@
+package com.PBW.RanTreker.Races;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class RaceUserController {
+    
+}
